6795512a3b865051101d99d20398fb3651855f8f
Stefan Boos <kontakt@boos.systems>
2024-01-12 21:29:06 +0100

    hello world

---
+++ README.md
@@ -0,0 +1,17 @@
+# Getting Started with Free Pascal
+
+## Prerequisites
+
+- Install the [Free Pascal Compiler](https://www.freepascal.org/)
+
+## Build
+
+```shell
+fpc hello
+```
+
+### Run
+
+```shell
+./hello
+```
---
+++ hello.pp
@@ -0,0 +1,10 @@
+(**
+ * Simple Hello World program.
+ *
+ * This program has been adopted from
+ * https://wiki.freepascal.org/Basic_Pascal_Tutorial/Hello,_World
+ **)
+program Hello;
+begin
+    writeln('Hello World!');
+end.
